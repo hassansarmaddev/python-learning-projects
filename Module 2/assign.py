1cb0fc18c09f5ea0c6dad280fbf287624f422222
Hassan Sarmad <mhassansarmad@gmail.com>
2025-04-20 00:31:07 -0700

    Mod2_complete

---
+++ Module 2/assign.py
@@ -0,0 +1,34 @@
+# belarus = medal_count["Belarus"]    #defining variable
+
+# countries = list(golds.keys())  # get a list of all the keys in the dictionary
+
+# golds["Spain"] = 2  # add Spain to the dictionary with value of 2
+
+# sports_periods["hockey"] = 3  # add hockey to the dictionary with value of 3
+# chile_golds = total_golds["Chile"]  # get the value associated with the key 'Chile' in the dictionary total_golds
+# print(chile_golds)
+
+# credits=sum(Junior.values()) # sum the values in the dictionary Junior
+
+
+
+
+# str1 = "peter piper picked a peck of pickled peppers"
+# freq = None
+
+# # YOUR CODE HERE
+# freq = {}
+# for character in str1:
+#     if character in freq:
+#         freq[character] += 1
+#     else:
+#         freq[character] = 1
+        
+# print(freq)        
+# #raise NotImplementedError()
+
+
+
+
+
+
