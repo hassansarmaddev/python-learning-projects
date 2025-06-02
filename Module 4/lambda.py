0cf28ca468ff1a675d9c670522b2e637252a9151
Hassan Sarmad <mhassansarmad@gmail.com>
2025-06-01 21:52:43 -0700

    update

---
+++ Module 4/lambda.py
@@ -0,0 +1,30 @@
+# def f(x):
+#     return x - 1
+
+# print(f)
+# print(type(f))
+# print(f(3))
+
+# print(lambda x: x-2)
+# print(type(lambda x: x-2))
+# print((lambda x: x-2)(6))
+
+
+
+
+# def last_char(s):
+#     return s[-1]
+# last_char = (lambda s: s[-1])
+
+
+
+
+# use 4 spaces for indentation
+
+# imports should go at the top of the file
+
+# separate function definitions with two blank lines
+
+# keep function definitions together
+
+# keep top level statements, including function calls, together at the bottom of the program
