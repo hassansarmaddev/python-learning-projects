ff5ef7ffdfa3f95fb1e77e10743468329f119447
Hassan <hassansarmaddev@gmail.com>
2025-07-10 17:19:04 -0700

    update

---
+++ Module 4/keywordParameters.py
@@ -0,0 +1,45 @@
+# names_scores = [("Jack",[67,89,91]),("Emily",[72,95,42]),("Taylor",[83,92,86])]
+# for name, scores in names_scores:
+#     print("The scores {nm} got were: {s1},{s2},{s3}.".format(nm=name,s1=scores[0],s2=scores[1],s3=scores[2]))
+
+
+
+
+
+
+
+
+
+# # this works
+# names = ["Jack","Jill","Mary"]
+# for n in names:
+#     print("'{}!' she yelled. '{}! {}, {}!'".format(n,n,n,"say hello"))
+
+# # but this also works!
+# names = ["Jack","Jill","Mary"]
+# for n in names:
+#     print("'{0}!' she yelled. '{0}! {0}, {1}!'".format(n,"say hello"))
+
+
+
+
+
+# initial = 7
+# def f(x, y = 3, z = initial):
+#     print("x, y, z are:", x, y, z)
+
+# f(2, 5)
+    
+    
+
+
+
+
+
+
+
+
+# def waste(mar, var = "Water", marble = "type"):
+#     final_string = var + " " + marble + " " + mar
+#     return final_string
+    
